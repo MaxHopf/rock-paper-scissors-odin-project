f7884e6e3f83c94d7aa68ceb730a4ca783163b20
Max Hopf <maxhopf@pm.me>
2022-02-17 17:02:32 +0100

    Adding playerplay() function which takes a user input to validate it and convert it appropiatly

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 //Enable strict mode
 "use strict";
 
+//Returns a random selection between rock, paper and scissors
 function computerPlay() {
     const randNum = Math.floor(Math.random() * 3);
     if (randNum == 0) {
@@ -12,4 +13,17 @@ function computerPlay() {
     } else {
         console.log("Math is wrong! Or was it just me?")
     }
+}
+//Returns a formated version of players selection after validating the user input
+function playerPlay() {
+    let playerChoice = prompt("Rock, Paper or Scissors?");
+    //Test if user input matches the requirements
+    while (!(/^rock$|^paper$|^scissors$/i.test(playerChoice))) {
+        playerChoice = prompt("Please type either \"Rock\", \"Paper\" or \"Scissors\"?");
+    }
+    return capitalize(playerChoice);
+}
+//Converts the first letter of a string to uppercase and the rest to lowercase 
+function capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
 }
