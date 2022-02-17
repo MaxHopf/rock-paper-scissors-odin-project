e65f8577d0c807363076b1af57b6659fd7f2261f
Max Hopf <maxhopf@pm.me>
2022-02-17 17:15:09 +0100

    Adding playRound() function which initiates a game

--- script.js
+++ script.js
@@ -27,3 +27,23 @@ function playerPlay() {
 function capitalize(string) {
     return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
 }
+//Function to initiate playing a game
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerPlay();
+    computerSelection = computerPlay();
+    if (playerSelection == "Rock" && computerSelection == "Paper") {
+        return "You Lose! Paper beats Rock";
+    } else if (playerSelection == "Scissors" && computerSelection == "Rock") {
+        return "You Lose! Rock beats Scissors";
+    } else if (playerSelection == "Paper" && computerSelection == "Scissors") {
+        return "You Lose! Scissors beats Paper";
+    } else if (playerSelection == "Paper" && computerSelection == "Rock") {
+        return "You Win! Paper beats Rock";
+    } else if (playerSelection == "Rock" && computerSelection == "Scissors") {
+        return "You Win! Rock beats Scissors";
+    } else if (playerSelection == "Scissors" && computerSelection == "Paper") {
+        return "You Win! Scissors beats Paper";
+    } else {
+        return "Remis!";
+    }
+}
