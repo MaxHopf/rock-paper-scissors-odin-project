c8de8b9996a1482173535caabd2b0e699536ac08
Max Hopf <maxhopf@pm.me>
2022-02-17 16:07:45 +0100

    A function to return a random selection between rock, paper, scissors was added

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
 //Enable strict mode
 "use strict";
+
+function computerPlay() {
+    const randNum = Math.floor(Math.random() * 3);
+    if (randNum == 0) {
+        return "Rock";
+    } else if (randNum == 1) {
+        return "Paper";
+    } else if (randNum == 2) {
+        return "Scissors";
+    } else {
+        console.log("Math is wrong! Or was it just me?")
+    }
+}
